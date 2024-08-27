5e90859270e8f5371ddb4178884d964c39ed9f85
Marco Dybig <marcodybig@hotmail.com>
2024-08-27 12:50:07 +0200

    Added properties

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    public class Demo
+    {
+        public DateTime Time { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
